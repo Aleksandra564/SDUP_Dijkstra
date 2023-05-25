aa151b85fc8fdea3ce849e710a4d6df70ddaa03a
Aleksandra564 <84809793+Aleksandra564@users.noreply.github.com>
2023-05-25 11:23:33 +0200

    Update Python_pseudocode.py
    Optymalizacja kodu i przekształcenie go na postać łatwą do zaimplementowania w Verilogu.

--- Python_pseudocode.py
+++ Python_pseudocode.py
@@ -1,34 +1,29 @@
 def Dijkstra(graph, source):
   graph_length = len(graph)
-  Q = [0]*graph_length
+  visited = [False]*graph_length
   counter = 0
   dist = [float("inf")]*graph_length
   prev = [None]*graph_length
 
-  for v in range(graph_length):
-    Q[v] = v
   dist[source] = 0
 
   while counter < graph_length:
     counter += 1
     min_dist = float("inf")
-    for v in range(len(Q)):
-      vertex = Q[v]
-      if dist[v] < min_dist and vertex != 9999:
+
+    for v in range(graph_length):
+      if not visited[v] and dist[v] < min_dist:
         min_dist = dist[v]
-        u = vertex               # przypisuje numer nieodwiedzonego wierzchołka o najmniejszym dist do zmiennej u
-    for i in range(len(Q)):
-      if Q[i] == u:
-        Q[i] = 9999
+        u = v               # przypisuje numer nieodwiedzonego wierzchołka o najmniejszym dist do zmiennej u
+    visited[u] = True
 
     for neighbor in range(graph_length):
       edge = graph[u][neighbor]
-      for j in Q:
-        if j == neighbor and edge != 0:
-          alt = dist[u] + edge
-          if alt < dist[neighbor]:
-            dist[neighbor] = alt
-            prev[neighbor] = u
+      if edge != 0:
+        alt = dist[u] + edge
+        if alt < dist[neighbor]:
+          dist[neighbor] = alt
+          prev[neighbor] = u
   
   return dist, prev
 
