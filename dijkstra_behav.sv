43aed102fa248432c10f8316e221d84933c6b4ef
Tomasz Jurczyk <49559807+Jurqa@users.noreply.github.com>
2023-06-01 01:50:40 +0200

    Update dijkstra_behav.sv
    Added another test input.

--- dijkstra_behav.sv
+++ dijkstra_behav.sv
@@ -1,4 +1,5 @@
 `timescale 1ns / 1ps
+
 //////////////////////////////////////////////////////////////////////////////////
 
 module dijkstra_behav();
@@ -8,14 +9,15 @@ module dijkstra_behav();
     // INPUT
     reg [N-1:0] source = 0;
     reg [N-1:0] graph [0:VERTICES-1][0:VERTICES-1] = '{'{0, 1, 1, 8, 0, 3, 0, 0, 0}, '{1, 0, 0, 0, 0, 0, 5, 0, 0}, '{1, 0, 0, 0, 0, 1, 0, 0, 0}, '{8, 0, 0, 0, 2, 0, 0, 0, 0}, '{0, 0, 0, 2, 0, 0, 0, 4, 0}, '{3, 0, 1, 0, 0, 0, 0, 2, 0}, '{0, 5, 0, 0, 0, 0, 0, 1, 1}, '{0, 0, 0, 0, 4, 2, 1, 0, 0}, '{0, 0, 0, 0, 0, 0, 1, 0, 0}};
-    
+    reg [N-1:0] graph2 [0:VERTICES-1][0:VERTICES-1] = '{'{0, 1, 0, 0, 5, 0, 0, 0, 0}, '{1, 0, 1, 7, 0, 0, 0, 0, 0}, '{0, 1, 0, 0, 1, 2, 0, 0, 0}, '{0, 7, 0, 0, 0, 0, 4, 0, 0}, '{5, 0, 1, 0, 0, 0, 0, 3, 0}, '{0, 0, 2, 0, 0, 0, 6, 4, 1}, '{0, 0, 0, 4, 0, 6, 0, 0, 0}, '{0, 0, 0, 0, 3, 4, 0, 0, 0}, '{0, 0, 0, 0, 0, 1, 0, 0, 0}};
+
     // OUTPUT
-    reg [N-1:0] distance [0:VERTICES-1] = '{default: 2**N -1};    // max mo¿liwa wartoœæ
+    reg [N-1:0] distance [0:VERTICES-1] = '{default: 2**N - 1};    // max moÅ¼liwa wartoÅ›Ä‡
     reg [N-1:0] prev [0:VERTICES-1] = '{default: 0};        // 0 zamiast None
     
     // OTHER VARIABLES
     reg visited [0:VERTICES-1] = '{default: 0};
-    reg [N-1:0] min_dist = 2**N -1;
+    reg [N-1:0] min_dist = 2**N - 1;
     reg [N-1:0] u;
     reg [N-1:0] edge_val;
     reg [N-1:0] alt;
@@ -27,10 +29,12 @@ module dijkstra_behav();
 //        foreach(graph[i]) $display("\t graph[%0d] = %0d", i, graph[i]);
 //        foreach(distance[i]) $display("\t distance[%0d] = %0d", i, distance[i]);
 //        foreach(prev[i]) $display("\t prev[%0d] = %0d", i, prev[i]);
+
+        //dla graph
         distance[source] = 0;
         
         for (i = 0; i < VERTICES; i = i + 1) begin
-            min_dist = 2**N -1;
+            min_dist = 2**N - 1;
             for (v = 0; v < VERTICES; v = v + 1) begin
                 if (visited[v] == 0 && distance[v] < min_dist) begin
                     min_dist = distance[v];
@@ -49,10 +53,46 @@ module dijkstra_behav();
                     end
                 end
             end
-            
         end
-        foreach(distance[i]) $display("\t distance[%0d] = %0d", i, distance[i]);
-        foreach(prev[i]) $display("\t prev[%0d] = %0d", i, prev[i]);
+        
+        $display("Results for graph:");
+        foreach (distance[i]) $display("\t distance[%0d] = %0d", i, distance[i]);
+        foreach (prev[i]) $display("\t prev[%0d] = %0d", i, prev[i]);
+        
+        //reset
+        visited = '{default: 0};
+        distance = '{default: 2**N - 1};
+        prev = '{default: 0};
+        min_dist = 2**N - 1;
+        
+        //graph2
+        distance[source] = 0;
+        
+        for (i = 0; i < VERTICES; i = i + 1) begin
+            min_dist = 2**N - 1;
+            for (v = 0; v < VERTICES; v = v + 1) begin
+                if (visited[v] == 0 && distance[v] < min_dist) begin
+                    min_dist = distance[v];
+                    u = v;
+                end
+            end
+            visited[u] = 1;
+            
+            for (neighbor = 0; neighbor < VERTICES; neighbor = neighbor + 1) begin
+                edge_val = graph2[u][neighbor];
+                if (edge_val != 0) begin
+                    alt = distance[u] + edge_val;
+                    if (alt < distance[neighbor]) begin
+                        distance[neighbor] = alt;
+                        prev[neighbor] = u;
+                    end
+                end
+            end
+        end
+        
+        $display("\nResults for graph2:");
+        foreach (distance[i]) $display("\t distance[%0d] = %0d", i, distance[i]);
+        foreach (prev[i]) $display("\t prev[%0d] = %0d", i, prev[i]);
     end
 
 endmodule
